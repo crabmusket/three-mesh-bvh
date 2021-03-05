a46e94c5a1f772493412bbac9c5903c3b8fe9da9
Daniel Buckmaster <daniel@crabmusket.net>
2021-03-06 10:42:15 +1100

    Add types for constants

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,14 @@ export function computeBoundsTree(): any
 export function disposeBoundsTree(): any
 export function acceleratedRaycast(): any
 
+export const CENTER: 0;
+export const AVERAGE: 1;
+export const SAH: 2;
+
+export const NOT_INTERSECTED: 0;
+export const INTERSECTED: 1;
+export const CONTAINED: 2;
+
 declare module 'three/src/core/BufferGeometry' {
 	interface BufferGeometry {
 		computeBoundsTree(): any;
