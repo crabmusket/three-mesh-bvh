63846826222a1f93e2364dca879d15a5369bfea6
Daniel Buckmaster <daniel@crabmusket.net>
2021-03-07 22:29:08 +1100

    Type for disposeBoundsTree

--- src/index.d.ts
+++ src/index.d.ts
@@ -8,7 +8,7 @@ export type Options = {
 
 type Strategies = typeof CENTER | typeof AVERAGE | typeof SAH;
 
-type HasBoundsTree = {boundsTree?: MeshBVH}
+type HasBoundsTree = {boundsTree?: MeshBVH | null}
 
 export class MeshBVH {}
 export class Visualizer {}
@@ -16,7 +16,7 @@ export class MeshBVHVisualizer {}
 export class MeshBVHDebug {}
 
 export function computeBoundsTree( this: HasBoundsTree, options?: Partial<Options> ): MeshBVH
-export function disposeBoundsTree(): unknown
+export function disposeBoundsTree( this: HasBoundsTree ): void
 export function acceleratedRaycast(): unknown
 export function estimateMemoryInBytes(): unknown
 export function getBVHExtremes(): unknown
@@ -31,7 +31,7 @@ export const CONTAINED: 2;
 
 declare module 'three/src/core/BufferGeometry' {
 	interface BufferGeometry {
-		boundsTree?: MeshBVH;
+		boundsTree?: MeshBVH | null;
 		computeBoundsTree: typeof computeBoundsTree;
 		disposeBoundsTree: typeof disposeBoundsTree;
 	}
